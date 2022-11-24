7cfa8488877f797e169e002ad81f4699e3421ab0
Allan Leles <111466705+changedcobra@users.noreply.github.com>
2022-11-23 21:20:06 -0300

    Aula8

---
+++ Aula08/olamundo.js
@@ -0,0 +1,17 @@
+console.log("Olá mundo!")
+//console.log é um comando que vai exibir 
+//informação no console do navegador */
+
+var Variavel1 = 2;
+//var tem o tipo de escopo global
+let Variavel2 = "Olá";
+//let tem o tipo de escopo local
+const Variavel3 = 3 + 3;
+//const é de escopo global e não pode ser
+console.log("A variavel1 é igual a ", Variavel1)
+Variavel1 = 4;
+console.log("Agora a variavel1 é igual a ", Variavel1)
+console.log("O valor de variavel2 é igual a ", Variavel2)
+Variavel2 = "agora vai";
+console.log("Agora o valor de variavel2 é ", Variavel2)
+console.log("Essa é a constante ", Variavel3)
